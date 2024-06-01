afab44b2da8d3581ea728afaed26957e2bfa4f95
okifxon <shokirjonovokifxon46@gmail.com>
2024-06-01 18:38:43 +0500

    mashina-hayda

---
+++ style.js
@@ -0,0 +1,11 @@
+let malumot = document.querySelector('.malumot')
+let button = document.querySelector('.next')
+// let malumott = document.querySelector('.malumot2')
+
+button && button.addEventListener("click", function (e){
+    e.preventDefault()
+    // malumot.style.display = 'none'
+    malumot.style.color = 'white'
+
+}) 
+    
